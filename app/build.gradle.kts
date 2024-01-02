56171bcf5fc17931c6d4fd105d6e1ddbf2df3006
dede888666 <t110360139@ntut.org.tw>
2024-01-02 22:46:29 +0800

    “third”

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.final_first"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.final_first"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
